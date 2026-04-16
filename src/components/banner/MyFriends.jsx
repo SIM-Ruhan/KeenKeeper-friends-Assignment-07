dc6989f04c388350e5c66bbab64a93b76221c465
SIM Ruhan <simruhan22@gmail.com>
2026-04-17 01:37:14 +0600

    Loading spinner added

--- src/components/banner/MyFriends.jsx
+++ src/components/banner/MyFriends.jsx
@@ -1,11 +1,26 @@
-import React, { use } from 'react';
-
-const appPromise = fetch("../public/Friend.json")
-.then((res) => res.json());
+import React, { useEffect, useState } from 'react';
 
 const MyFriends = () => {
-    const friends = use(appPromise);
-    console.log(friends);
+     const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("../public/Friend.json")
+            .then(res => res.json())
+            .then(data => {
+                setFriends(data);
+                setLoading(false);
+            });
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="flex justify-center items-center h-[60vh]">
+                <span className="loading loading-spinner text-error"></span>
+            </div>
+        );
+    }
+
     return (
         <div className='w-[85%] mx-auto'>
             <h1 className='text-2xl font-semibold mt-10 mb-4'>Your Friends</h1>
@@ -17,10 +32,18 @@ return(
         <img src={friend.picture} alt="friend" className='w-20 h-20 rounded-full' />
         <h2 className='text-xl font-semibold my-3'>{friend.name}</h2>
         <p className='text-[12px] text-[#64748B]'>{friend.days_since_contact}d ago</p>
-        <button className='rounded-full bg-[#CBFADB] p-2 text-[#244D3F] my-3'>{friend.tags}</button>
+<div className="flex flex-wrap gap-2 my-3">
+  {friend.tags.map((tag, index) => (
+    <span 
+      key={index} 
+      className="bg-[#CBFADB] text-[#244D3F] px-3 py-1 rounded-full text-sm">
+      {tag}
+    </span>
+  ))}
+</div>
 
         <div className="">
-            <button className='bg-[#EFAD44] text-white p-2 rounded-full'>{friend.status}</button>
+            <button className='bg-[#EFAD44] text-white px-3 py-1 rounded-full'>{friend.status}</button>
             </div>
     </div>
 
